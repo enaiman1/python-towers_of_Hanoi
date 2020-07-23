30c6f5eb4686fac9cf3043b19bf23fb070dca99e
Ericcentrik <enaiman1@gmail.com>
2020-07-23 13:09:01 -0400

    starting to build logic for the game

---
+++ script.py
@@ -0,0 +1,14 @@
+from stack import Stack
+
+print("\nLet's play Towers of Hanoi!!")
+
+#Create the Stacks
+stacks = []
+
+left_stack = Stack("Left")
+middle_stack = Stack('Middle')
+right_stack = Stack("Right")
+
+stacks.append(left_stack)
+stacks.append(middle_stack)
+stacks.append(right_stack)
